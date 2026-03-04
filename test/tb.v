4d81aa68e6bc393f2703b0997d6f50e18533a74a
ewa231 <107299101+ewa231@users.noreply.github.com>
2026-03-04 19:36:08 +0100

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -14,25 +14,17 @@ module tb ();
   end
 
   // Wire up the inputs and outputs:
-  reg clk;
-  reg rst_n;
-  reg ena;
-  reg [7:0] ui_in;
-  reg [7:0] uio_in;
-  wire [7:0] uo_out;
-  wire [7:0] uio_out;
-  wire [7:0] uio_oe;
+  wire clk;
+  wire rst;
+  wire photon_in;
+  reg [1:0] signal_out;
 
   // Replace tt_um_photomultiplier_accum with your module name:
   tt_um_photomultiplier_accum user_project (
-      .ui_in  (ui_in),    // Dedicated inputs
-      .uo_out (uo_out),   // Dedicated outputs
-      .uio_in (uio_in),   // IOs: Input path
-      .uio_out(uio_out),  // IOs: Output path
-      .uio_oe (uio_oe),   // IOs: Enable path (active high: 0=input, 1=output)
-      .ena    (ena),      // enable - goes high when design is selected
       .clk    (clk),      // clock
-      .rst_n  (rst_n)     // not reset
+      .rst    (rst),     // reset
+      .photon_in (photon_in),
+      .signal_out (signal_out)
   );
 
 endmodule
